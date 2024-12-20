228ec6812a6fecf1216e35fe8e300ef03be43185
Senorihl <rodolphe.renaux@gmail.com>
2024-12-20 15:08:41 +0100

    add assets building with tailwind

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,10 +9,21 @@ if test -n "$DEV_UID"; then
   groupmod -g ${DEV_UID} www-data
 fi
 
+if test -n "$NODE_DEV_UID"; then
+  usermod -u ${NODE_DEV_UID} www-data
+  groupmod -g ${NODE_DEV_UID} www-data
+fi
+
 if [ $1 = "supervisord" ] || [ $1 = "php" ]; then
   git config --global --add safe.directory /var/www/app
   composer install --prefer-dist --no-progress --no-scripts --no-interaction
-  chown -R www-data:www-data /var/www/app/
+  chown -R www-data:www-data /var/www/
+fi
+
+if [ $1 = "yarn" ]; then
+  git config --global --add safe.directory /var/www/app
+  yarn install
+  chown -R node:node /var/www/
 fi
 
 exec "$@"
---
+++ src/Phalcon/Volt/AssetExtension.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Phalcon\Volt;
+
+class AssetExtension
+{
+    /**
+     * @var array
+     */
+    private static array $assetMap = [];
+
+    public function initialize()
+    {
+
+        if (file_exists(realpath(BASE_PATH . '/public/assets/manifest.json'))) {
+            $content = file_get_contents(BASE_PATH . '/public/assets/manifest.json');
+            $content = json_decode($content, true);
+
+            if (json_last_error() === JSON_ERROR_NONE) {
+                self::$assetMap = $content;
+            }
+        }
+    }
+
+    public function compileFunction(string $name, string $arguments)
+    {
+        if (in_array($name, ['js_entry_tag', 'css_entry_tag']) && method_exists(get_called_class(), $name)) {
+            return sprintf('%s::%s(%s)', get_called_class(), $name, $arguments);
+        }
+    }
+
+    public static function js_entry_tag(string $name)
+    {
+        if (!empty(self::$assetMap[$name])) {
+            return sprintf('<script type="application/javascript" src="%s"></script>', htmlentities(self::$assetMap[$name]));
+        }
+    }
+
+    public static function css_entry_tag(string $name)
+    {
+        if (!empty(self::$assetMap[$name])) {
+            return sprintf('<link rel="stylesheet" href="%s" />', htmlentities(self::$assetMap[$name]));
+        }
+    }
+
+}
