18e1d52d1f4d3f7f7b38fc78e94b5ff108a9dec2
Senorihl <rodolphe.renaux@gmail.com>
2025-05-29 11:34:06 +0200

    WIP

--- src/Phalcon/Mvc/Model/MetaData/Strategy/Annotations.php
+++ src/Phalcon/Mvc/Model/MetaData/Strategy/Annotations.php
@@ -2,6 +2,7 @@
 
 namespace App\Phalcon\Mvc\Model\MetaData\Strategy;
 
+use Phalcon\Annotations\Annotation;
 use Phalcon\Di\DiInterface;
 use Phalcon\Mvc\Model\MetaData;
 use Phalcon\Mvc\ModelInterface;
@@ -51,118 +52,134 @@ class Annotations implements \Phalcon\Mvc\Model\MetaData\Strategy\StrategyInterf
         $reflection = $this->getAnnotations()->get(get_class($model));
         $collection = $reflection->getClassAnnotations();
 
-        if ($collection) {
-            if ($collection->has('Index')) {
-                $arguments = $collection->get('Index')->getArguments();
-                $name = $arguments['name'];
+        foreach ($collection->getAnnotations() as $annotation) {
+            /** @var Annotation $annotation */
+            switch ($annotation->getName()) {
+                case 'Index': {
+                    $arguments = $annotation->getArguments();
+                    $name = $arguments['name'];
 
-                if (!is_array($arguments['columns'])) {
-                    $arguments['columns'] = [$arguments['columns']];
+                    if (!is_array($arguments['columns'])) {
+                        $arguments['columns'] = [$arguments['columns']];
+                    }
+
+                    if (isset($arguments['unique'])) {
+                        $metaData['unique'][$name] = $arguments['columns'];
+                    } else {
+                        $metaData['indexes'][$name] = $arguments['columns'];
+                    }
+                    break;
                 }
-
-                if (isset($arguments['unique'])) {
-                    $metaData['unique'][$name] = $arguments['columns'];
+                case 'HasMany': {
+                    $this->getManager()->addHasMany($model, ...$annotation->getArguments());
+                    break;
                 }
-            }
-
-            if ($collection->has('HasMany')) {
-                $this->getManager()->addHasMany($model, ...$collection->get('HasMany')->getArguments());
-            }
-
-            if ($collection->has('HasManyToMany')) {
-                $this->getManager()->addHasManyToMany($model, ...$collection->get('HasManyToMany')->getArguments());
-            }
-
-            if ($collection->has('HasOne')) {
-                $this->getManager()->addHasOne($model, ...$collection->get('HasOne')->getArguments());
-            }
-
-            if ($collection->has('BelongsTo')) {
-                $this->getManager()->addBelongsTo($model, ...$collection->get('BelongsTo')->getArguments());
-            }
-
-            if ($collection->has('HasOneThrough')) {
-                $this->getManager()->addHasOneThrough($model, ...$collection->get('HasOneThrough')->getArguments());
+                case 'HasManyToMany': {
+                    $this->getManager()->addHasManyToMany($model, ...$annotation->getArguments());
+                    break;
+                }
+                case 'HasOne': {
+                    $this->getManager()->addHasOne($model, ...$annotation->getArguments());
+                    break;
+                }
+                case 'BelongsTo': {
+                    $this->getManager()->addBelongsTo($model, ...$annotation->getArguments());
+                    break;
+                }
+                case 'HasOneThrough': {
+                    $this->getManager()->addHasOneThrough($model, ...$annotation->getArguments());
+                    break;
+                }
             }
         }
 
         foreach ($reflection->getPropertiesAnnotations() as $name => $collection) {
-            if ($collection->has('BelongsTo')) {
-                $arguments = $collection->get('BelongsTo')->getArguments();
-                $referencedModelName = array_shift($arguments);
-                $referencedColumn = array_shift($arguments);
-                $options = array_shift($arguments) ?? [];
+            foreach ($collection->getAnnotations() as $annotation) {
+                /** @var Annotation $annotation */
+                switch ($annotation->getName()) {
+                    case 'BelongsTo': {
+                        $arguments = $annotation->getArguments();
+                        $referencedModelName = array_shift($arguments);
+                        $referencedColumn = array_shift($arguments);
+                        $options = array_shift($arguments) ?? [];
 
-                if (!in_array(\Phalcon\Mvc\Model::class, class_parents($referencedModelName))) {
-                    throw new \Exception('Unable to reference a non-Model object ' . $referencedModelName . ' in ' . get_class($model));
-                }
+                        if (!in_array(\Phalcon\Mvc\Model::class, class_parents($referencedModelName))) {
+                            throw new \Exception('Unable to reference a non-Model object ' . $referencedModelName . ' in ' . get_class($model));
+                        }
 
-                $this->getManager()->addBelongsTo($model, $name, $referencedModelName, $referencedColumn, $options);
-            } elseif ($collection->has('HasOne')) {
-                $arguments = $collection->get('HasOne')->getArguments();
-                $referencedModelName = array_shift($arguments);
-                $referencedColumn = array_shift($arguments);
-                $options = array_shift($arguments) ?? [];
-
-                if (!in_array(\Phalcon\Mvc\Model::class, class_parents($referencedModelName))) {
-                    throw new \Exception('Unable to reference a non-Model object ' . $referencedModelName . ' in ' . get_class($model));
-                }
-
-                $this->getManager()->addHasOne($model, $name, $referencedModelName, $referencedColumn, $options ?? []);
-            } elseif ($collection->has('HasMany')) {
-                $arguments = $collection->get('HasMany')->getArguments();
-                $referencedModelName = array_shift($arguments);
-                $referencedColumn = array_shift($arguments);
-                $options = array_shift($arguments) ?? [];
-
-                if (!in_array(\Phalcon\Mvc\Model::class, class_parents($referencedModelName))) {
-                    throw new \Exception('Unable to reference a non-Model object ' . $referencedModelName . ' in ' . get_class($model));
-                }
-
-                $this->getManager()->addHasOne($model, $name, $referencedModelName, $referencedColumn, $options ?? []);
-            }
-
-            if ($collection->has('Column')) {
-                $arguments = $collection->get('Column')->getArguments();
-                if (isset($arguments['size'])) {
-                    $metaData['sizes'][$propertyMap[$name]] = intval($arguments['size']);
-                }
-                if (isset($arguments['sql_default'])) {
-                    $metaData['sql_default'][$propertyMap[$name]] = $arguments['sql_default'];
-                }
-                if (isset($arguments['unique'])) {
-                    if (!is_array($arguments['unique'])) {
-                        $arguments['unique'] = [$arguments['unique']];
+                        $this->getManager()->addBelongsTo($model, $name, $referencedModelName, $referencedColumn, $options);
+                        break;
                     }
+                    case 'HasOne': {
+                        $arguments = $annotation->getArguments();
+                        $referencedModelName = array_shift($arguments);
+                        $referencedColumn = array_shift($arguments);
+                        $options = array_shift($arguments) ?? [];
 
-                    foreach ($arguments['unique'] as $partsOf) {
-                        if (is_string($partsOf)) {
-                            if (!array_key_exists($partsOf, $metaData['unique'])) {
-                                $metaData['unique'][$partsOf] = [];
+                        if (!in_array(\Phalcon\Mvc\Model::class, class_parents($referencedModelName))) {
+                            throw new \Exception('Unable to reference a non-Model object ' . $referencedModelName . ' in ' . get_class($model));
+                        }
+
+                        $this->getManager()->addHasOne($model, $name, $referencedModelName, $referencedColumn, $options);
+                        break;
+                    }
+                    case 'HasMany': {
+                        $arguments = $annotation->getArguments();
+                        $referencedModelName = array_shift($arguments);
+                        $referencedColumn = array_shift($arguments);
+                        $options = array_shift($arguments) ?? [];
+
+                        if (!in_array(\Phalcon\Mvc\Model::class, class_parents($referencedModelName))) {
+                            throw new \Exception('Unable to reference a non-Model object ' . $referencedModelName . ' in ' . get_class($model));
+                        }
+
+                        $this->getManager()->addHasMany($model, $name, $referencedModelName, $referencedColumn, $options);
+                        break;
+                    }
+                    case 'Column': {
+                        $arguments = $annotation->getArguments();
+                        if (isset($arguments['size'])) {
+                            $metaData['sizes'][$propertyMap[$name]] = intval($arguments['size']);
+                        }
+                        if (isset($arguments['sql_default'])) {
+                            $metaData['sql_default'][$propertyMap[$name]] = $arguments['sql_default'];
+                        }
+                        if (isset($arguments['unique'])) {
+                            if (!is_array($arguments['unique'])) {
+                                $arguments['unique'] = [$arguments['unique']];
                             }
-                            $metaData['unique'][$partsOf][] = $propertyMap[$name];
-                        } elseif (boolval($partsOf) === true) {
-                            if (!array_key_exists($propertyMap[$name] . '_uniq', $metaData['unique'])) {
-                                $metaData['unique'][$propertyMap[$name] . '_uniq'] = [];
+
+                            foreach ($arguments['unique'] as $partsOf) {
+                                if (is_string($partsOf)) {
+                                    if (!array_key_exists($partsOf, $metaData['unique'])) {
+                                        $metaData['unique'][$partsOf] = [];
+                                    }
+                                    $metaData['unique'][$partsOf][] = $propertyMap[$name];
+                                } elseif (boolval($partsOf) === true) {
+                                    if (!array_key_exists($propertyMap[$name] . '_uniq', $metaData['unique'])) {
+                                        $metaData['unique'][$propertyMap[$name] . '_uniq'] = [];
+                                    }
+                                    $metaData['unique'][$propertyMap[$name] . '_uniq'][] = $propertyMap[$name];
+                                }
                             }
-                            $metaData['unique'][$propertyMap[$name] . '_uniq'][] = $propertyMap[$name];
+                        }
+                        if (isset($arguments['index'])) {
+                            if (!is_array($arguments['index'])) {
+                                $arguments['index'] = [$arguments['index']];
+                            }
+
+                            foreach ($arguments['index'] as $partsOf) {
+                                if (is_string($partsOf)) {
+                                    if (!array_key_exists($partsOf, $metaData['indexes'])) {
+                                        $metaData['indexes'][$partsOf] = [];
+                                    }
+                                    $metaData['indexes'][$partsOf][] = $propertyMap[$name];
+                                }
+                            }
                         }
                     }
                 }
-                if (isset($arguments['index'])) {
-                    if (!is_array($arguments['index'])) {
-                        $arguments['index'] = [$arguments['index']];
-                    }
 
-                    foreach ($arguments['index'] as $partsOf) {
-                        if (is_string($partsOf)) {
-                            if (!array_key_exists($partsOf, $metaData['indexes'])) {
-                                $metaData['indexes'][$partsOf] = [];
-                            }
-                            $metaData['indexes'][$partsOf][] = $propertyMap[$name];
-                        }
-                    }
-                }
             }
         }
 
--- tests/Phalcon/Mvc/Model/MetaData/Strategy/AnnotationsTest.php
+++ tests/Phalcon/Mvc/Model/MetaData/Strategy/AnnotationsTest.php
@@ -13,6 +13,7 @@ use Phalcon\Mvc\Model;
 use Phalcon\Mvc\Model\ManagerInterface;
 use Phalcon\Mvc\Model\MetaData;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
@@ -28,9 +29,30 @@ class AnnotationsTest extends TestCase
         Di::getDefault()->set('modelsManager', $this->modelsManager = $this->createMock(ManagerInterface::class));
     }
 
+    public function testGetRootStrategy()
+    {
+        $this->assertInstanceOf(\Phalcon\Mvc\Model\MetaData\Strategy\Annotations::class, (new Annotations)->getRootStrategy());
+    }
+
     public function testGetMetaData()
     {
-        $strategy = $this->createPartialMock(Annotations::class, ['getRootStrategy']);
+        $strategy = $this->createPartialMock(Annotations::class, ['getRootStrategy', 'getColumnMaps']);
+
+        $strategy
+            ->method('getColumnMaps')
+            ->willReturn([
+                [],
+                [
+                    'test_size' => 'test_size',
+                    'test_index' => 'test_index',
+                    'test_unique' => 'test_unique',
+                    'test_anon_unique' => 'test_anon_unique',
+                    'test_now' => 'test_now',
+                    'test_belongs' => 'test_belongs',
+                    'test_hasMany' => 'test_hasMany',
+                    'test_hasOne' => 'test_hasOne',
+                ]
+            ]);
 
         $strategy
             ->expects($this->any())
@@ -56,33 +78,95 @@ class AnnotationsTest extends TestCase
             ->expects($this->any())
             ->method('getPropertiesAnnotations')
             ->willReturn([
-                'test' => ($annotations = $this->createMock(Collection::class)),
-                'test_bis' => $annotations,
+                'test_size' => $withLength = $this->createMock(Collection::class),
+                'test_index' => $withIndex = $this->createMock(Collection::class),
+                'test_unique' => $withUnique = $this->createMock(Collection::class),
+                'test_anon_unique' => $withAnonUnique = $this->createMock(Collection::class),
+                'test_now' => $withDefault = $this->createMock(Collection::class),
+                'test_belongs' => $withBelongs = $this->createMock(Collection::class),
+                'test_hasMany' => $withMany = $this->createMock(Collection::class),
+                'test_hasOne' => $withOne = $this->createMock(Collection::class),
             ]);
 
         $classAnnotations
-            ->method('has')
-            ->willReturnMap([
-                ['Index', true],
-                ['HasMany', true],
-                ['HasManyToMany', true],
-                ['HasOne', true],
-                ['BelongsTo', true],
-                ['HasOneThrough', true],
+            ->method('getAnnotations')
+            ->willReturn([
+                $index = $this->createMock(Annotation::class),
+                $index,
+                $many = $this->createMock(Annotation::class),
+                $many2many = $this->createMock(Annotation::class),
+                $one = $this->createMock(Annotation::class),
+                $belongs = $this->createMock(Annotation::class),
+                $oneThrough = $this->createMock(Annotation::class)
             ]);
 
-        $classAnnotations
-            ->method('get')
-            ->willReturnMap([
-                ['Index', $index = $this->createMock(Annotation::class)],
-                ['HasMany', $many = $this->createMock(Annotation::class)],
-                ['HasManyToMany', $many2many = $this->createMock(Annotation::class)],
-                ['HasOne', $one = $this->createMock(Annotation::class)],
-                ['BelongsTo', $belongs = $this->createMock(Annotation::class)],
-                ['HasOneThrough', $oneThrough = $this->createMock(Annotation::class)],
-            ]);
+        $withLength->method('getAnnotations')->willReturn([$column = $this->createMock(Annotation::class)]);
+        $withIndex->method('getAnnotations')->willReturn([$column]);
+        $withUnique->method('getAnnotations')->willReturn([$column]);
+        $withAnonUnique->method('getAnnotations')->willReturn([$column]);
+        $withDefault->method('getAnnotations')->willReturn([$column]);
+        $withBelongs->method('getAnnotations')->willReturn([$column_belongs = $this->createMock(Annotation::class)]);
+        $withMany->method('getAnnotations')->willReturn([$column_many = $this->createMock(Annotation::class)]);
+        $withOne->method('getAnnotations')->willReturn([$column_one = $this->createMock(Annotation::class)]);
 
-        $index->method('getArguments')->willReturn(['name' => 'test_index', 'columns' => 'test_column', 'unique' => true]);
+        $column
+            ->expects($this->atLeast(3))
+            ->method('getName')
+            ->willReturn('Column');
+
+        $column
+            ->expects($this->atLeast(3))
+            ->method('getArguments')
+            ->willReturnOnConsecutiveCalls(
+                ['size' => 3],
+                ['index' => 'columned_index'],
+                ['unique' => 'columned_unique'],
+                ['unique' => true],
+                ['sql_default' => 'NOW()'],
+            );
+
+        $column_belongs
+            ->expects($this->atLeast(1))
+            ->method('getName')
+            ->willReturn('BelongsTo');
+
+        $column_belongs
+            ->expects($this->atLeast(1))
+            ->method('getArguments')
+            ->willReturn([ get_class($this->createMock(\Phalcon\Mvc\Model::class)), 'id' ]);
+
+        $column_many
+            ->expects($this->atLeast(1))
+            ->method('getName')
+            ->willReturn('HasMany');
+
+        $column_many
+            ->expects($this->atLeast(1))
+            ->method('getArguments')
+            ->willReturn([ get_class($this->createMock(\Phalcon\Mvc\Model::class)), 'id' ]);
+
+        $column_one
+            ->expects($this->atLeast(1))
+            ->method('getName')
+            ->willReturn('HasOne');
+
+        $column_one
+            ->expects($this->atLeast(1))
+            ->method('getArguments')
+            ->willReturn([ get_class($this->createMock(\Phalcon\Mvc\Model::class)), 'id' ]);
+
+
+        $index->method('getName')->willReturn('Index');
+        $many->method('getName')->willReturn('HasMany');
+        $many2many->method('getName')->willReturn('HasManyToMany');
+        $one->method('getName')->willReturn('HasOne');
+        $belongs->method('getName')->willReturn('BelongsTo');
+        $oneThrough->method('getName')->willReturn('HasOneThrough');
+
+        $index->method('getArguments')->willReturnOnConsecutiveCalls(
+            ['name' => 'test_index', 'columns' => 'test_column', 'unique' => true],
+            ['name' => 'test_index_bis', 'columns' => 'test_bis'],
+        );
         $many->method('getArguments')->willReturn(['','','']);
         $many2many->method('getArguments')->willReturn(['','','','','','']);
         $one->method('getArguments')->willReturn(['','','']);
@@ -91,12 +175,163 @@ class AnnotationsTest extends TestCase
 
         $model = $this->createMock(Model::class);
 
+        $this->modelsManager->expects($this->exactly(2))->method('addHasMany')->withAnyParameters();
+        $this->modelsManager->expects($this->once())->method('addHasManyToMany')->withAnyParameters();
+        $this->modelsManager->expects($this->exactly(2))->method('addHasOne')->withAnyParameters();
+        $this->modelsManager->expects($this->exactly(2))->method('addBelongsTo')->withAnyParameters();
+        $this->modelsManager->expects($this->once())->method('addHasOneThrough')->withAnyParameters();
+
+        $metaData = $strategy->getMetaData(
+            $model,
+            $this->createMock(DiInterface::class),
+        );
+
+        $this->assertSame(array (
+                'unique' =>
+                    array (
+                        'test_index' =>
+                            array (
+                                0 => 'test_column',
+                            ),
+                        'columned_unique' =>
+                            array (
+                                0 => 'test_unique',
+                            ),
+                        'test_anon_unique_uniq' =>
+                            array (
+                                0 => 'test_anon_unique',
+                            ),
+                    ),
+                'indexes' =>
+                    array (
+                        'test_index_bis' =>
+                            array (
+                                0 => 'test_bis',
+                            ),
+                        'columned_index' =>
+                            array (
+                                0 => 'test_index',
+                            ),
+                    ),
+                'sizes' =>
+                    array (
+                        'test_size' => 3,
+                    ),
+                'sql_default' =>
+                    array (
+                        'test_now' => 'NOW()',
+                    ),
+            )
+            , $metaData);
+    }
+
+    public static function getInvalidRelations(): array
+    {
+        return [
+            ['test_belongs'],
+            ['test_hasMany'],
+            ['test_hasOne'],
+        ];
+    }
+
+    /**
+     * @return void
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     */
+    #[DataProvider('getInvalidRelations')]
+    public function testGetMetaDataInvalidRelatedModel(string $invalidRelationColumn)
+    {
+        $strategy = $this->createPartialMock(Annotations::class, ['getRootStrategy', 'getColumnMaps']);
+
+        $strategy
+            ->method('getColumnMaps')
+            ->willReturn([
+                [],
+                [
+                    'test_belongs' => 'test_belongs',
+                    'test_hasMany' => 'test_hasMany',
+                    'test_hasOne' => 'test_hasOne',
+                ]
+            ]);
+
+        $strategy
+            ->expects($this->any())
+            ->method('getRootStrategy')
+            ->willReturn($oStrategy = $this->createMock(MetaData\Strategy\StrategyInterface::class));
+
+        $oStrategy
+            ->expects($this->once())
+            ->method('getMetaData')
+            ->willReturn([]);
+
+        $this->annotations
+            ->expects($this->any())
+            ->method('get')
+            ->willReturn($reflection = $this->createMock(Reflection::class));
+
+        $reflection
+            ->expects($this->any())
+            ->method('getClassAnnotations')
+            ->willReturn($classAnnotations = $this->createMock(Collection::class));
+
+        $reflection
+            ->expects($this->any())
+            ->method('getPropertiesAnnotations')
+            ->willReturn([
+                'test_belongs' => $withBelongs = $this->createMock(Collection::class),
+                'test_hasMany' => $withMany = $this->createMock(Collection::class),
+                'test_hasOne' => $withOne = $this->createMock(Collection::class),
+            ]);
+
+        $classAnnotations
+            ->method('getAnnotations')
+            ->willReturn([]);
+
+        $withBelongs->method('getAnnotations')->willReturn([$column_belongs = $this->createMock(Annotation::class)]);
+        $withMany->method('getAnnotations')->willReturn([$column_many = $this->createMock(Annotation::class)]);
+        $withOne->method('getAnnotations')->willReturn([$column_one = $this->createMock(Annotation::class)]);
+
+        $column_belongs
+            ->method('getName')
+            ->willReturn('BelongsTo');
+
+        $column_belongs
+            ->method('getArguments')
+            ->willReturn([ $invalidRelationColumn === 'test_belongs' ? \stdClass::class : get_class($this->createMock(\Phalcon\Mvc\Model::class)), 'id' ]);
+
+        $column_many
+            ->method('getName')
+            ->willReturn('HasMany');
+
+        $column_many
+            ->method('getArguments')
+            ->willReturn([ $invalidRelationColumn === 'test_hasMany' ? \stdClass::class : get_class($this->createMock(\Phalcon\Mvc\Model::class)), 'id' ]);
+
+        $column_one
+            ->method('getName')
+            ->willReturn('HasOne');
+
+        $column_one
+            ->method('getArguments')
+            ->willReturn([ $invalidRelationColumn === 'test_hasOne' ? \stdClass::class : get_class($this->createMock(\Phalcon\Mvc\Model::class)), 'id' ]);
+
+
+        $model = $this->createMock(Model::class);
+
+        $this->modelsManager->expects($this->any())->method('addHasMany')->withAnyParameters();
+        $this->modelsManager->expects($this->never())->method('addHasManyToMany')->withAnyParameters();
+        $this->modelsManager->expects($this->any())->method('addHasOne')->withAnyParameters();
+        $this->modelsManager->expects($this->any())->method('addBelongsTo')->withAnyParameters();
+        $this->modelsManager->expects($this->never())->method('addHasOneThrough')->withAnyParameters();
+
+        $this->expectExceptionMessageMatches('#^Unable to reference a non-Model object stdClass in MockObject_Model_#mi');
+
         $strategy->getMetaData(
             $model,
             $this->createMock(DiInterface::class),
         );
 
-        $this->assertTrue(true);
+
     }
 
     public function testGetColumnMaps()
